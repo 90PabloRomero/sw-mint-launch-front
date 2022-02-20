cc18de6b5e93266717314197c9b2522872f4857e
Pablo R  Romero <46016525+90PabloRomero@users.noreply.github.com>
2022-02-20 17:21:47 -0300

    correct address

--- src/util/api.js
+++ src/util/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// using aws ip 52.15.191.202
-
 export default axios.create({
-  baseURL: process.env.API_HOST || "http://localhost:5000",
+  baseURL: "https://backend.spaceworms.app",
   headers: {
     "Content-Type": "application/json",
   },
