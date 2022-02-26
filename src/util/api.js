e6e3840fad0620da9e83057228d5174ff3b813dc
Pablo Romero <90pabloromero@gmail.com>
2022-02-26 20:44:05 -0300

    cambio contrato test mainnetaddress

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://backend.spaceworms.app",
+  // baseURL: "https://backend.spaceworms.app",// lo cambiamos para el test
+  baseURL: "http://18.228.213.2:3001", // sa aws
   headers: {
     "Content-Type": "application/json",
   },
